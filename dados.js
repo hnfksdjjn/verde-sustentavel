c8fa8dc6f936f6195ef3d196e854d7ac5b69edb0
Edson francisco da silva <106998449+hnfksdjjn@users.noreply.github.com>
2024-09-07 11:25:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,40 @@
+
+
+let dados = [
+    {
+      titulo: "Reciclagem de Plástico",
+      descricao: "A reciclagem mecânica é o método mais utilizado e consiste em quatro etapas: moagem, lavagem, secagem e extrusão. O resultado é um material granulado que pode ser usado para produzir novos produtos plásticos, como embalagens, pisos, mangueiras e sacos de lixo.",
+      link: "https://www.plastico.com.br/reciclagem-do-plastico/",
+      tags: "reciclagem plástico meio ambiente",
+      imagem: "src/reciclagem_plastica (1).jpg"
+    },
+    {
+      titulo: "Economia de Energia",
+      descricao: "São analisadas as perspectivas de preços de energéticos, com ênfase em estudos de competitividade relativa em termos internacionais e desdobramentos nos preços internos de energia. A gestão da inovação é outra vertente abrangida neste conjunto de estudos.",
+      link: "https://www.cemig.com.br/usina-do-conhecimento/23-dicas-para-economizar-na-conta-de-luz/",
+      tags: "energia economia sustentabilidade eficiência energética",
+      imagem: "src/Economia_Energia_1 (1).jpg"
+    },
+    {
+      titulo: "Alimentação Sustentável",
+      descricao: "A alimentação sustentável pode ser definida como a adoção de medidas de baixa agressão ao meio ambiente. Além disso, ela também pode oferecer benefícios para a saúde. Por exemplo: redução no consumo de proteína animal, aumento do consumo de vegetais e prioridade ao pequeno produtor.",
+      link: "https://panelanestle.com.br/alimentacao-sustentavel/#:~:text=A%20alimenta%C3%A7%C3%A3o%20sustent%C3%A1vel%20pode%20ser,e%20prioridade%20ao%20pequeno%20produtor.",
+      tags: "alimentação dieta desperdício de alimentos",
+      imagem: "src/Alimentação_Sustentável.jpg"
+    },
+    {
+      titulo: "Mobilidade Urbana",
+      descricao: "Mobilidade Urbana é definida como a condição que permite o deslocamento das pessoas em uma cidade, com o objetivo de desenvolver relações sociais e econômicas.",
+      link: "https://mobilidadeurbana.mpba.mp.br/mobilidade-urbana/#:~:text=Mobilidade%20Urbana%20%C3%A9%20definida%20como,%E2%80%9Cfacilidade%20para%20se%20mover%E2%80%9D.",
+      tags:  "transporte mobilidade pegada de carbono",
+      imagem: "src/Mobilidade-Urbana_1.jpg"
+    },
+    {
+      titulo: "Conservação da Água",
+      descricao: "Conservação de Água constitui qualquer ação que: Reduz a quantidade de água extraída das fontes de suprimento; • Reduz o consumo de água; • Reduz o desperdício de água; • Reduz as perdas de água; • Aumenta a eficiência do uso da água; • Aumenta a reutilização da água; • Evita a poluição da água.",
+      link: "https://www.casal.al.gov.br/conservacao-da-agua/",
+      tags: "água agua conservação uso responsável",
+      imagem: "src/Conservação-da-agua.jpg"
+    }
+];
+  
